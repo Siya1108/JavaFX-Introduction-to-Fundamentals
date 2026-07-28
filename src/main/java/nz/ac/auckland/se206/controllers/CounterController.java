3c0b608068c203df1543bcbae6028cb1f383c233
Qianwei Zhong <qzho174@aucklanduni.ac.nz>
2026-07-28 15:58:51 +1200

    CounterController: create code for the buttons

--- src/main/java/nz/ac/auckland/se206/controllers/CounterController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/CounterController.java
@@ -1,5 +1,50 @@
 package nz.ac.auckland.se206.controllers;
 
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class CounterController {
+    private int count = 0;
+
+
+    private void updateLabel() {
+        numberLabel.setText(Integer.toString(count));
+    }
+
+    @FXML
+    private Button incrementButton;
+    @FXML
+    private Button decrementButton;
+    @FXML
+    private Button   resetButton;
+    @FXML
+    private Button switchButton;
+    @FXML
+    private Label numberLabel;
     
+    @FXML
+    private void increment() {
+        count++;
+        updateLabel();
+    }
+
+    @FXML
+    private void decrement() {
+        count--;
+        updateLabel();
+    }   
+
+    @FXML
+    private void reset() {
+        count = 0;
+        updateLabel();
+    }
+
+    @FXML
+    private void switchToMusicPlayer(ActionEvent event) {
+        System.out.println(event.getSource().getClass());
+        System.out.println(event.getSource());
+    }
 }
