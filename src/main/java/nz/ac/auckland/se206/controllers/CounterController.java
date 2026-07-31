1e15d298cec8ed52b2520fb7f530f6c6bdc6f4b4
Qianwei Zhong <qzho174@aucklanduni.ac.nz>
2026-07-31 12:42:55 +1200

    Fix some bugs

--- src/main/java/nz/ac/auckland/se206/controllers/CounterController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/CounterController.java
@@ -1,56 +1,58 @@
 package nz.ac.auckland.se206.controllers;
 
-import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import nz.ac.auckland.se206.App;
 
 public class CounterController {
-    private int count = 0;
+  private int count = 0;
 
+  private void updateLabel() {
+    numberLabel.setText(Integer.toString(count));
+  }
 
-    private void updateLabel() {
-        numberLabel.setText(Integer.toString(count));
+  @FXML private Button incrementButton;
+  @FXML private Button decrementButton;
+  @FXML private Button resetButton;
+  @FXML private Button switchButton;
+  @FXML private Label numberLabel;
+
+  @FXML
+  private void increment() {
+    count++;
+    updateLabel();
+  }
+
+  @FXML
+  private void decrement() {
+    count--;
+    updateLabel();
+  }
+
+  @FXML
+  private void reset() {
+    count = 0;
+    updateLabel();
+  }
+
+  @FXML
+  private void switchToMusicPlayer(ActionEvent event) {
+    Button button = (Button) event.getSource();
+    Scene sceneButtonIsIn = button.getScene();
+
+    try {
+      sceneButtonIsIn.setRoot(App.loadFXML("musicplayer"));
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-    @FXML
-    private Button incrementButton;
-    @FXML
-    private Button decrementButton;
-    @FXML
-    private Button   resetButton;
-    @FXML
-    private Button switchButton;
-    @FXML
-    private Label numberLabel;
-    
-    @FXML
-    private void increment() {
-        count++;
-        updateLabel();
-    }
-
-    @FXML
-    private void decrement() {
-        count--;
-        updateLabel();
-    }   
-
-    @FXML
-    private void reset() {
-        count = 0;
-        updateLabel();
-    }
-
-    @FXML
-    private void switchToMusicPlayer(ActionEvent event) {
-        Button button = (Button) event.getSource();
-        Scene sceneButtonIsIn = button.getScene();
-
-        try {
-            sceneButtonIsIn.setRoot(App.loadFXML("musicplayer"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+  @FXML
+  private void initialize() {
+    // Initialization code for the music player scene
+    System.out.println("*** Initializing Music Controller *** " + this);
+  }
 }
